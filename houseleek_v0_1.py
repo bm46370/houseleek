8d16eefbfcb44565ae9812fc1c79531b1bce707d
Bruno Matijak <bruno.matijak@gmail.com>
2018-02-07 17:23:39 +0000

    Update houseleek_v0_1.py
    added PIR sensor, changed to recognise bird and then play song, also using interupt

--- houseleek_v0_1.py
+++ houseleek_v0_1.py
@@ -1,5 +1,8 @@
 #### TODO: reftactor code in clases...; pir senzor ; KiCad shema
 
+#za zvuk - puštanje preko aux od raspberry-a
+import pygame
+
 import picamera
 import time
 import RPi.GPIO as GPIO
@@ -13,8 +16,21 @@ from clarifai import rest
 from clarifai.rest import ClarifaiApp
 from clarifai.rest import Image as ClImage
 import sys
-print (sys.version)
+##print (sys.version)
 
+GPIO.setmode(GPIO.BCM)  
+# GPIO 17 set up as input. 
+pir_sensor = 17 #GPIO pin 17
+kill_switch=22
+camera_led=18
+pir_led=27
+
+GPIO.setup(pir_sensor, GPIO.IN, GPIO.PUD_DOWN)
+GPIO.setup(kill_switch, GPIO.IN, GPIO.PUD_DOWN)
+GPIO.setup(camera_led, GPIO.OUT)
+GPIO.setup(pir_led,GPIO.OUT)
+
+##sending mail, not used at the moment
 def SendMail(FileName, danger):
     msg = MIMEMultipart()
     msg['Subject'] = 'Houseleek Surveilance'
@@ -46,44 +62,48 @@ def SendMail(FileName, danger):
     s.sendmail("from@gmail.com", "to@gmail.com", msg.as_string())
     s.quit()
 
-#take picture
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(18, GPIO.OUT)
-
-#GPIO.output(18, GPIO.HIGH)
-camera = picamera.PiCamera()
-#camera.capture('slika.jpg')
-#GPIO.output(18, GPIO.LOW)
-	
-camera.vflip = True
-
-GPIO.output(18, GPIO.HIGH)
-camera.capture('slika2.jpg')
-GPIO.output(18, GPIO.LOW)
-
-GPIO.output(18, GPIO.HIGH)
-camera.start_recording('video.mjpeg')
-time.sleep(5)
-camera.stop_recording()
-GPIO.output(18, GPIO.LOW)
-
-camera.close()
-GPIO.cleanup()
-
-#recognition api
-danger = False
-app = ClarifaiApp(api_key='+++++++key+++++++')
-model = app.models.get('general-v1.3')
-image = ClImage(file_obj=open('slika2.jpg', 'rb'))
-response=model.predict([image])
-concepts = response['outputs'][0]['data']['concepts']
-for concept in concepts:
-    print(concept['name'], concept['value'])
-    if any(x in concept['name'] for x in ["danger","man","people","adult","weapon","safety"]):
-        danger = True
-        print('OPASNOST')
-if danger:
-    print('OPASNOST')
+def my_callback(channel):
+    GPIO.output(pir_led,True) #Turn on LED
+    time.sleep(0.5) # leave LED on for 0.5 seconds, just for test
     
-#sent mail
-SendMail(['slika2.jpg','video.mjpeg'], danger)
+    camera = picamera.PiCamera()
+    camera.vflip = True
+
+    GPIO.output(camera_led, GPIO.HIGH)
+    camera.capture('slika.jpg')
+    GPIO.output(camera_led, GPIO.LOW)
+    camera.close()
+    GPIO.output(pir_led,False) #turn off LED
+    
+    danger = False
+    app = ClarifaiApp(api_key='------------key-----------')
+    model = app.models.get('general-v1.3')
+    image = ClImage(file_obj=open('slika.jpg', 'rb'))
+    response=model.predict([image])
+    concepts = response['outputs'][0]['data']['concepts']
+    for concept in concepts:
+        print(concept['name'], concept['value'])
+        if any(x in concept['name'] for x in ["bird"]): # can be more filters: ["danger","man","people","adult","weapon","safety"]):
+            danger = True
+            #print('bird on camera')
+    if danger:
+        print('sviraj')
+        #zvuk
+        song = "countdown.mp3"
+        pygame.init()
+        pygame.mixer.init()
+        pygame.mixer.music.load(song)
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy():
+            pygame.time.Clock().tick(10)
+
+
+print "During this waiting time, your computer is not"
+print "wasting resources by polling for a PIR.\n"
+GPIO.add_event_detect(pir_sensor, GPIO.RISING, callback=my_callback) 
+try:
+    GPIO.wait_for_edge(kill_switch, GPIO.RISING)
+    GPIO.cleanup() 
+    print "\nKill switch pressed"
+except KeyboardInterrupt:
+    GPIO.cleanup()       # clean up GPIO on CTRL+C exit  
